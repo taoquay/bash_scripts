aabbf61fe09625519cd7004f4af2eb7f90de22b7
taoquay <taoquay@gmail.com>
2014-04-29 21:28:55 -0400

    Bug fix to resolve issue when file name contains a space

--- extract_audio.sh
+++ extract_audio.sh
@@ -12,12 +12,12 @@
 for i in *.{flv,mp4}; do
 	AUDIO_TYPE=`ffprobe "$i" 2>&1 | grep -o 'Audio: \w\{3\}'`
 	AUDIO_TYPE=${AUDIO_TYPE: -3}
-	FREQUENCY=`ffprobe $i 2>&1 | grep -o '.\{6\}Hz' | head -c 5`
+	FREQUENCY=`ffprobe "$i" 2>&1 | grep -o '.\{6\}Hz' | head -c 5`
 
-	BITRATE=`ffprobe $i 2>&1 | grep -o '\d\{2,3\} kb\/s (default)' | grep -o '^\d\{2,3\}'`
+	BITRATE=`ffprobe "$i" 2>&1 | grep -o '\d\{2,3\} kb\/s (default)' | grep -o '^\d\{2,3\}'`
 
 	# Set minimum bitrate to 64 kb/s
-	if [ "$BITRATE" -lt "64" ]
+	if [ $BITRATE -lt "64" ]
 	then
 		BITRATE="64"
 	fi
@@ -28,13 +28,13 @@ for i in *.{flv,mp4}; do
 
 	if [ $AUDIO_TYPE == "aac" ]
 	then
-		ffmpeg -i "$i" -vn -acodec copy $M4A_FILE 2>/dev/null \
-		&& ffmpeg -i $M4A_FILE -acodec libmp3lame -ab $BITRATE -ar $FREQUENCY $MP3_FILE 2>/dev/null \
+		ffmpeg -i "$i" -vn -acodec copy "$M4A_FILE" 2>/dev/null \
+		&& ffmpeg -i "$M4A_FILE" -acodec libmp3lame -ab $BITRATE -ar $FREQUENCY "$MP3_FILE" 2>/dev/null \
 		&& mv "$i" ./done \
-		&& rm $M4A_FILE \
+		&& rm "$M4A_FILE" \
 		&& ECHO "`date +%T`: Conversion of \"$i\" completed."
 	else
-		ffmpeg -i "$i" -vn -acodec copy $MP3_FILE 2>/dev/null \
+		ffmpeg -i "$i" -vn -acodec copy "$MP3_FILE" 2>/dev/null \
 		&& mv "$i" ./done \
 		&& ECHO "`date +%T`: Conversion of \"$i\" completed."
 	fi
